9ed68f74d03eb722d3b31c2de539474b3656f5e0
tiffany-philippi <tiffanyphilippi15@gmail.com>
2023-01-10 17:18:04 -0300

    adding banner to home page

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <img className='banner-img' src="/img/banner.png" alt="Main Banner of this page"/>
+        </header>
+    );
+}
+
+export default Banner;
